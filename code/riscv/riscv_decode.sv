06c6fa41bfe4a5f106654f6a53470177fe4b71da
Shraddha Sahoo <39151337+shraddha375@users.noreply.github.com>
2026-08-08 18:31:31 +0530

    Update riscv_decode.sv

--- code/riscv/riscv_decode.sv
+++ code/riscv/riscv_decode.sv
@@ -1,30 +1,32 @@
+`include "riscv_defs.sv"
+
 module riscv_decode
 (
-    input             [31:0] instr_i,
+    input                   [31:0] instr_i,
 
-    output                   is_alu_o,        // R-type or I-type ALU op
-    output                   is_load_o,
-    output                   is_store_o,
-    output                   is_branch_o,
-    output                   is_jal_o,
-    output                   is_jalr_o,
-    output                   invalid_o,       // unrecognized opcode
+    output logic                   is_alu_o,        // R-type or I-type ALU op
+    output logic                   is_load_o,
+    output logic                   is_store_o,
+    output logic                   is_branch_o,
+    output logic                   is_jal_o,
+    output logic                   is_jalr_o,
+    output logic                   invalid_o,       // unrecognized opcode
 
-    output [`ALU_OP_W - 1:0] alu_op_o,
-    output                   alu_src_b_imm_o, // 1 = operand_b comes from immediate, not rb
-    output                   alu_src_a_pc_o,  // 1 = operand_a comes from PC, not rs1
+    output logic [`ALU_OP_W - 1:0] alu_op_o,
+    output logic                   alu_src_b_imm_o, // 1 = operand_b comes from immediate, not rb
+    output logic                   alu_src_a_pc_o,  // 1 = operand_a comes from PC, not rs1
 
-    output             [4:0] rd_o,
-    output             [4:0] rs1_o,
-    output             [4:0] rs2_o,
-    output                   rd_valid_o,      // does this instruction write rd?
+    output logic             [4:0] rd_o,
+    output logic             [4:0] rs1_o,
+    output logic             [4:0] rs2_o,
+    output logic                   rd_valid_o,      // does this instruction write rd?
 
-    output             [2:0] branch_funct3_o,
+    output logic             [2:0] branch_funct3_o,
 
-    output             [1:0] mem_size_o,      // 00=byte, 01=half, 10=word
-    output                   mem_unsigned_o,  // 1 = zero-extend (LBU/LHU), 0 = sign-extend
+    output logic             [1:0] mem_size_o,      // 00=byte, 01=half, 10=word
+    output logic                   mem_unsigned_o,  // 1 = zero-extend (LBU/LHU), 0 = sign-extend
 
-    output            [31:0] imm_o            // sign-extended immediate, already assembled
+    output logic            [31:0] imm_o            // sign-extended immediate
 );
 
     always_comb begin
@@ -56,25 +58,32 @@ module riscv_decode
                             alu_op_o = `ALU_ADD;
                         else if (instr_i[`FUNCT7_R] == `FUNCT7_SUB)
                             alu_op_o = `ALU_SUB;
+                        else
+                            invalid_o = 1'b1;
                     end
                     
                     `FUNCT3_SLT     : alu_op_o = `ALU_SLT;
-                    `FUNCT3_AND     : alu_op_o = `ALU_AND;
-                    `FUNCT3_OR      : alu_op_o = `ALU_OR;
-                    `FUNCT3_XOR     : alu_op_o = `ALU_XOR;
-                    
-                    
-                    `FUNCT3_SLL     : alu_op_o = `ALU_SLL;
+
                     `FUNCT3_SLTU    : alu_op_o = `ALU_SLTU;
 
+                    `FUNCT3_AND     : alu_op_o = `ALU_AND;
+
+                    `FUNCT3_OR      : alu_op_o = `ALU_OR;
+
+                    `FUNCT3_XOR     : alu_op_o = `ALU_XOR;
+
+                    `FUNCT3_SLL     : alu_op_o = `ALU_SLL;
+
                     `FUNCT3_SRL_SRA : begin 
-                        if (instr_i[`FUNCT7_R] == `FUNCT7_ADD)
+                        if (instr_i[`FUNCT7_R] == `FUNCT7_SRL)
                             alu_op_o = `ALU_SRL;
-                        else if (instr_i[`FUNCT7_R] == `FUNCT7_SUB)
+                        else if (instr_i[`FUNCT7_R] == `FUNCT7_SRA)
                             alu_op_o = `ALU_SRA;
+                        else
+                            invalid_o = 1'b1;
                     end
 
-                    default         : ;
+                    default         : invalid_o = 1'b1;
                 endcase
             end
 
@@ -84,22 +93,30 @@ module riscv_decode
                 alu_src_b_imm_o = 1'b1;
 
                 case (instr_i[`FUNCT3_R])
-                    `FUNCT3_ADD_SUB : alu_op_o = `ALU_ADD;   
+                    `FUNCT3_ADD_SUB : alu_op_o = `ALU_ADD;
+
                     `FUNCT3_SLT     : alu_op_o = `ALU_SLT;
-                    `FUNCT3_AND     : alu_op_o = `ALU_AND;
-                    `FUNCT3_XOR     : alu_op_o = `ALU_XOR;
-                    `FUNCT3_OR      : alu_op_o = `ALU_OR;
+
                     `FUNCT3_SLTU    : alu_op_o = `ALU_SLTU;
 
+                    `FUNCT3_AND     : alu_op_o = `ALU_AND;
+                    
+                    `FUNCT3_XOR     : alu_op_o = `ALU_XOR;
+                    
+                    `FUNCT3_OR      : alu_op_o = `ALU_OR;
+                    
                     `FUNCT3_SLL     : alu_op_o = `ALU_SLL;
+                    
                     `FUNCT3_SRL_SRA : begin
-                        if (instr_i[`FUNCT7_R] == `FUNCT7_ADD)
+                        if (instr_i[`FUNCT7_R] == `FUNCT7_SRL)
                             alu_op_o = `ALU_SRL;
-                        else if (instr_i[`FUNCT7_R] == `FUNCT7_SUB)
+                        else if (instr_i[`FUNCT7_R] == `FUNCT7_SRA)
                             alu_op_o = `ALU_SRA;
+                        else
+                            invalid_o = 1'b1;
                     end
 
-                    default         : ;
+                    default         : invalid_o = 1'b1;
                 endcase
             end
             
@@ -134,6 +151,8 @@ module riscv_decode
                         mem_size_o     = 2'b00;
                         mem_unsigned_o = 1'b1;
                     end 
+
+                    default        : invalid_o = 1'b1; 
                 endcase
             end
 
@@ -157,6 +176,8 @@ module riscv_decode
                         mem_size_o     = 2'b00;
                         mem_unsigned_o = 1'b0;
                     end
+
+                    default        : invalid_o = 1'b1; 
                 endcase
             end
                 
@@ -164,18 +185,22 @@ module riscv_decode
                 is_branch_o = 1'b1;
                 
                 case (instr_i[`FUNCT3_R])
-                    `FUNCT3_BEQ  : alu_op_o = `ALU_SUB;
-                    `FUNCT3_BNE  : alu_op_o = `ALU_SUB;
-                    `FUNCT3_BLT  : alu_op_o = `ALU_SLT;
-                    `FUNCT3_BGE  : alu_op_o = `ALU_SLT;
-                    `FUNCT3_BLTU : alu_op_o = `ALU_SLTU;
-                    `FUNCT3_BGEU : alu_op_o = `ALU_SLTU;
+                    `FUNCT3_BEQ  : alu_op_o  = `ALU_SUB;
+                    `FUNCT3_BNE  : alu_op_o  = `ALU_SUB;
+                    `FUNCT3_BLT  : alu_op_o  = `ALU_SLT;
+                    `FUNCT3_BGE  : alu_op_o  = `ALU_SLT;
+                    `FUNCT3_BLTU : alu_op_o  = `ALU_SLTU;
+                    `FUNCT3_BGEU : alu_op_o  = `ALU_SLTU;
+                    default      : invalid_o = 1'b1;
                 endcase
             end
 
             `OPCODE_JAL    : begin 
-                is_jal_o   = 1'b1;
-                rd_valid_o = 1'b1;
+                is_jal_o        = 1'b1;
+                rd_valid_o      = 1'b1;
+                alu_src_a_pc_o  = 1'b1;
+                alu_src_b_imm_o = 1'b1;
+                alu_op_o        = `ALU_ADD;
             end
 
             `OPCODE_JALR   : begin
@@ -201,8 +226,6 @@ module riscv_decode
             end
 
             default        : invalid_o   = 1'b1;
-            
-
         endcase
     end
 
@@ -211,8 +234,6 @@ module riscv_decode
     assign rs2_o = instr_i[`RS2_R];
 
     always_comb begin
-        imm_o = 32'b0;
-
         case (instr_i[`OPCODE_R])
             `OPCODE_OP_IMM  : imm_o = {{20{instr_i[31]}}, instr_i[31:20]};
             `OPCODE_LOAD    : imm_o = {{20{instr_i[31]}}, instr_i[31:20]};
